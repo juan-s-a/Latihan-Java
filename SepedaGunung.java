56d0d8af022548e21845de0a095a02edcef4b8ac
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:17:26 -0700

    Update SepedaGunung.java

--- SepedaGunung.java
+++ SepedaGunung.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan;
 
-/**
- *
- * @author asus
- */
-public class SepedaGunung {
-    
+public class SepedaGunung extends sepeda {
+    int setSadel;
+    public void setSadel(int nilaiSadel) {
+        setSadel = nilaiSadel;
+        System.out.println("Tinggi Sadel = " + setSadel);
+    }
 }
