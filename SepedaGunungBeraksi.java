8743d0a0549f61a4c657aaacae4d002d34bde2d9
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:17:48 -0700

    Update SepedaGunungBeraksi.java

--- SepedaGunungBeraksi.java
+++ SepedaGunungBeraksi.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan;
 
-/**
- *
- * @author asus
- */
 public class SepedaGunungBeraksi {
-    
+   public static void main(String[] args) {
+       SepedaGunung sepedaku = new SepedaGunung();
+       sepedaku.tambahKecepatan(10);
+       sepedaku.ubahGir(2);
+       sepedaku.setSadel(20);
+   }
 }
