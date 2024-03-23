c1c3cd6000f0efb79c60e749baeb36c8c20f5d17
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:16:50 -0700

    Update Polimorfisme.java

--- Polimorfisme.java
+++ Polimorfisme.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan;
 
-/**
- *
- * @author asus
- */
+class hewan {
+    public void suara() {
+        System.out.println("Kuda mengikik...");
+    }
+}
+class kuda extends hewan {
+    public void suara () {
+        System.out.println("Kuda mengikik...");
+    }
+}
+class kucing extends hewan {
+    public void suara () {
+        System.out.println("Kucing mengeong...");
+    }
+}
+class ayam extends hewan {
+    public void suara () {
+        System.out.println("Ayam berkokok...");
+    }
+}
 public class Polimorfisme {
-    
+    public static void main(String[] args) {
+        hewan HEWAN = new hewan();
+        kuda KUDA = new kuda();
+        kucing KUCING = new kucing();
+        ayam AYAM = new ayam();
+        
+        HEWAN.suara();
+        
+        HEWAN = KUDA;
+        HEWAN.suara();
+        
+        HEWAN = KUCING;
+        HEWAN.suara();
+        
+        HEWAN = AYAM;
+        HEWAN.suara();
+    }
 }
