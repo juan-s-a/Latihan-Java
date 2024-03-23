f615d1f666ae256056e99f45161fa2764d3298d1
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:18:12 -0700

    Update sepeda.java

--- sepeda.java
+++ sepeda.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package latihan;
 
-/**
- *
- * @author asus
- */
 public class sepeda {
+    int kecepatan = 0;
+    int gir = 0;
     
+    void ubahGir(int pertambahanGir) {
+        gir = gir + pertambahanGir;
+        System.out.println("Gir: " + gir);
+    }
+    void tambahKecepatan(int pertambahanKecepatan) {
+        kecepatan = kecepatan + pertambahanKecepatan;
+        System.out.println("Kecepatan: " + kecepatan);
+    }
 }
