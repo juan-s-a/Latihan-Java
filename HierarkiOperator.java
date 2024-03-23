16951e28718a362bb956984113e32524489c6dd3
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:13:41 -0700

    Update HierarkiOperator.java

--- HierarkiOperator.java
+++ HierarkiOperator.java
@@ -9,5 +9,12 @@ package latihan;
  * @author asus
  */
 public class HierarkiOperator {
-    
+    public static void main(String[] args) {
+        int A, B;
+        
+        A = 8 + 2 * 3 / 6;
+        B = (8 + 2) * 3 / 6;
+        System.out.println("Hasil dari A = " + A);
+        System.out.println("Hasil dari B = " + B);
+    }
 }
