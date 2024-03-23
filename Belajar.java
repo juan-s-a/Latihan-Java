a775fe0b7e3adb82c7fb7b9663c2c0a22a619489
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-03-22 22:09:19 -0700

    Add files via upload

---
+++ Belajar.java
@@ -0,0 +1,5 @@
+package latihan;
+public class Belajar {
+    public String x = "Pintar";
+    public String y = "Java";
+}
---
+++ Latihan.java
@@ -0,0 +1,9 @@
+package latihan;
+public class Latihan {
+    public static void main(String[] args) {
+        int radius = 30;
+        final double PI = 3.14;
+        double circumference = 2 * PI * radius;
+        System.out.println("Keliling Lingkaran: "+circumference);
+    }    
+}
