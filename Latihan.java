5898c99f1a166efccec218a134d466defd43b3e1
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-04-05 03:00:18 -0700

    Delete Latihan.java

--- Latihan.java
+++
@@ -1,9 +0,0 @@
-package latihan;
-public class Latihan {
-    public static void main(String[] args) {
-        int radius = 30;
-        final double PI = 3.14;
-        double circumference = 2 * PI * radius;
-        System.out.println("Keliling Lingkaran: "+circumference);
-    }    
-}
