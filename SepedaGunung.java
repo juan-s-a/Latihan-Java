a8fea7228321e555a05a6a71edcf5c0516071acc
juan-s-a <164319109+juan-s-a@users.noreply.github.com>
2024-04-05 03:01:34 -0700

    Delete SepedaGunung.java

--- SepedaGunung.java
+++
@@ -1,9 +0,0 @@
-package latihan;
-
-public class SepedaGunung extends sepeda {
-    int setSadel;
-    public void setSadel(int nilaiSadel) {
-        setSadel = nilaiSadel;
-        System.out.println("Tinggi Sadel = " + setSadel);
-    }
-}
